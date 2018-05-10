45f01a154ff99cd8dc7dfd40ebc80b35b3471507
Unknown <mcwvproductions@gmail.com>
2018-05-10 11:00:29 -0700

    ex basic structure add

--- master/master.ino
+++ master/master.ino
@@ -1,9 +1,16 @@
 void setup() {
   // put your setup code here, to run once:
-
+  allActive();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+bool allActive() {
+  //CHECK PLAYER UNIT
+  //CHECK DM UNIT
+  //WAIT IF FAIL THEN LOOP
+  return true;
 }
