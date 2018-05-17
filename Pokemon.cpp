37a3aab8c4a11cffb3affab3333ac7fe61974c10
Connor Garrett <mcwvproductions@gmail.com>
2018-05-17 10:42:19 -0700

    basic pokemon class added

---
+++ Pokemon.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Pokemon
+{
+    public:
+        string type;
+        string type2;
+        string name;
+        int maxHP;
+        int currentHP;
+        string move1;
+        string move2;
+        string move3;
+        string move4;
+        Pokemon();
+};
+
+/*Pokemon::Pokemon(string typep, string namep, int maxHPp) {
+    type = typep;
+    name = namep;
+    maxHP = maxHPp;
+    move1 = "tackle";
+}*/
